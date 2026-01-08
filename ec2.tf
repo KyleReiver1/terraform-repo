2746cf0b6952e49301471d05bdfb03533ededdbe
SuperfluousApps <matt@skylighttechnology.com>
2026-01-08 17:31:31 +0000

    day3

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "example" {
   ami           = "ami-068c0051b15cdb816"
   instance_type = "t3.micro"
+
+  vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
+  
   tags = {
     "Name" = "kyle-vm1"
   }
